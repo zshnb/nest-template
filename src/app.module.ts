7193f7670f4924def6cbfebd43cbf1b0107d8c70
zshnb <a857681664@gmail.com>
2023-09-21 11:38:43 +0800

    setup cls

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from './configuration';
+import { ClsModule } from 'nestjs-cls';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true
+    }),
+    ClsModule.forRoot({
+      middleware: {
+        mount: true,
+        setup: (cls, req) => {
+          cls.set('userId', req.headers['x-user-id']);
+        },
+      },
     }),
   ],
   controllers: [AppController],
